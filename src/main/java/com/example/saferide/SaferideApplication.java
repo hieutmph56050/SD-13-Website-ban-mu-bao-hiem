8a510e931766274a886fb1e054304d1cbd73adcf
PhamAnhTuan121 <phamtuan785aa@gmail.com>
2024-10-07 17:49:21 +0700

    Create Folder SafeRide

---
+++ src/main/java/com/example/saferide/SaferideApplication.java
@@ -0,0 +1,13 @@
+package com.example.saferide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaferideApplication.class, args);
+    }
+
+}
